709d3e092f88ab8dd92bf380e0c27f2258d2be99
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-07-11 19:41:54 +0100

    i make some basics

---
+++ 1-Computers-and-Programs/basic.py
@@ -0,0 +1,16 @@
+print("Hello, World")
+
+print(2 + 3)
+
+print("2 + 3 =", 2 + 3)
+
+def hello():
+    print("hello")
+    print("Computer is Fun!")
+hello()
+
+
+def greet(person):
+    print("Hello", person)
+    print("How are you?")
+greet("adnane")
