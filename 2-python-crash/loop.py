fc5fdb8f58a9fcfa74d1dd48a6a187a7ea798724
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-09-07 23:06:49 +0100

    i make break loop

--- 2-python-crash/loop.py
+++ 2-python-crash/loop.py
@@ -648,3 +648,14 @@ while active:
     else:
         print(message)
 
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\nEnter 'quit' when you are finish quit"
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print("I'd love to go to " + city.title() + "!")
+
