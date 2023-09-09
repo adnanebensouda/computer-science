a145d93edd1ab6f69f611f3caf35a749c7821e82
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-09-09 07:02:43 +0100

    first ex done

--- 2-python-crash/functions.py
+++ 2-python-crash/functions.py
@@ -2,4 +2,23 @@ def greet_user(username):
     """Display a simple greeting."""
     print("Hello " + username.title() + "!")
 
+
 greet_user('jesse')
+
+"""first ex"""
+
+
+def display_message():
+    print("\ni learn how to create function")
+
+
+display_message()
+
+
+def favorite_book(title):
+    print("my favorite book is " + title.title() + ".")
+
+
+favorite_book('python Crack course')
+
+
