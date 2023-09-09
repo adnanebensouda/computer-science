c7bc710dc5fcf5799df55b0584d6b94e08c014b3
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-09-09 16:15:33 +0100

    my second ex

--- 2-python-crash/functions.py
+++ 2-python-crash/functions.py
@@ -22,3 +22,33 @@ def favorite_book(title):
 favorite_book('python Crack course')
 
 
+def describe_pet(pet_name, animal_type='dog'):
+    """Display information about a pet."""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+
+# A dog named Willie.
+describe_pet('willie')
+describe_pet(pet_name='willie')
+# A hamster named Harry.
+describe_pet('harry', 'hamster')
+describe_pet(pet_name='harry', animal_type='hamster')
+describe_pet(animal_type='hamster', pet_name='harry')
+
+"""second ex"""
+
+print("\n")
+
+def make_shirt(text, size):
+    print("A " + size + " shirt will be printed with the message: " + text)
+
+
+make_shirt('hello world', 'medium')
+print("\n")
+make_shirt(size='large', text='i love python')
+
+
+
+
+
