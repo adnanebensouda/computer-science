376880044e2098c9228c5e6052427afaebebb223
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-09-10 12:45:32 +0100

    return dic

--- 2-python-crash/functions.py
+++ 2-python-crash/functions.py
@@ -59,3 +59,43 @@ def describe_city(city, country='morocco'):
 describe_city('Reykjavik', 'Iceland')
 describe_city('antwerp', 'belgium')
 describe_city('tanger')
+
+
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+
+musician = get_formatted_name('jimi', 'hendrix')
+print("\n" + musician + "\n")
+
+
+
+
+def get_formatted_name(first_name, last_name, middle_name=''):
+    """Return a full name, neatly formatted."""
+    if middle_name:
+        full_name = first_name + ' ' + middle_name + ' ' + last_name
+    else:
+        full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+musician = get_formatted_name('john', 'lee', 'hooker')
+print(musician + "\n")
+
+
+def build_person(first_name, last_name, age=''):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+
+musician = build_person('jimi', 'handrix', age='27')
+print(musician)
+
