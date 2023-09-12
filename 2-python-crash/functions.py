a9d1636d6df3ab299ef50fe5efb492123a70f8ac
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-09-12 05:19:56 +0100

    list with function

--- 2-python-crash/functions.py
+++ 2-python-crash/functions.py
@@ -184,6 +184,10 @@ while True:
     print("Album information:")
     print(album)
 
+"""from here"""
+
+print("\n")
+
 
 def greet_users(names):
     for name in names:
@@ -194,4 +198,51 @@ def greet_users(names):
 usernames = ['hannah', 'ty', 'margot']
 greet_users(usernames)
 
+print("\n")
 
+# Start with some designs that need to be printed.
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+# Simulate printing each design, until none are left.
+# Move each design to completed_models after printing.
+
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    # Simulate creating a 3D print from the design.
+    print("Printing model: " + current_design)
+    completed_models.append(current_design)
+
+# Display all completed models.
+
+print("\nThe following models have been printed:")
+
+for completed_model in completed_models:
+    print(completed_model)
+
+"""from here second function"""
+
+print("\n")
+
+
+def print_models(unprinted_designs, completed_models):
+    """
+     Simulate printing each design, until none are left.
+     Move each design to completed_models after printing.
+     """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print("Printing model: " + current_design)
+        completed_models.append(current_design)
+
+
+def show_completed_models(completed_models):
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
