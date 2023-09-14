94422b1ec3283bdfe21cb4708d4acfab9dd2f259
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-09-14 16:15:29 +0100

    i make function that return tuples

--- 2-python-crash/functions.py
+++ 2-python-crash/functions.py
@@ -281,3 +281,18 @@ magician = ['Shin Lim', 'David Copperfield', 'Matt Franco', 'Penn & Teller']
 make_great(magician)
 
 show_magicians(magician)
+
+print("\n")
+
+
+def make_pizza(*toppings):
+    """Print the list of toppings that have been requested."""
+    """Summarize the pizza we are about to make."""
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
