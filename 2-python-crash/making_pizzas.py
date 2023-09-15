65d29b94ececb8cc08e7a66540d6819044894736
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-09-16 00:06:11 +0100

    understand the moduls

--- 2-python-crash/making_pizzas.py
+++ 2-python-crash/making_pizzas.py
@@ -1,4 +1,10 @@
 import pizza
+from target import greet_users
 
 pizza.make_pizza(16, 'pepperoni')
 pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+print("\n")
+
+usernames = ['hannah', 'ty', 'margot']
+greet_users(usernames)
