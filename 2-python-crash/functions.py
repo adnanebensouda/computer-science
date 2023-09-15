dae9bdc02ab75502e54abf83ca7d13d7ec4c2bc6
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-09-15 20:18:25 +0100

    i just finish def

--- 2-python-crash/functions.py
+++ 2-python-crash/functions.py
@@ -285,14 +285,25 @@ show_magicians(magician)
 print("\n")
 
 
-def make_pizza(*toppings):
+def make_pizza(size, *toppings):
     """Print the list of toppings that have been requested."""
     """Summarize the pizza we are about to make."""
-    print("\nMaking a pizza with the following toppings:")
+    print("\Making a " + str(size) + "-inch pizza with the following topping:")
     for topping in toppings:
         print("- " + topping)
 
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
 
+
+def build_profile(first, last, **user_info):
+    profile = {}
+    profile['first_name'] = first
+    profile['last_name'] = last
+    for key, value in user_info.items():
+        profile[key] = value
+    return profile
+
+
+user_profile = build_profile
