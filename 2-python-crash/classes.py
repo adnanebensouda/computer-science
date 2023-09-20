9604ef47529db21a84ad506de3d0ff5111bc7720
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-09-20 23:47:54 +0100

    class i just add

--- 2-python-crash/classes.py
+++ 2-python-crash/classes.py
@@ -8,3 +8,7 @@ class Dog():
 
     def roll_over(self):
         print(self.name.title() + " rolled over!")
+
+my_dog = Dog('willie', 6)
+print("My dog's name is " + my_dog.name.title() + ".")
+print("My dog is " + str(my_dog.age) + " years old.")
